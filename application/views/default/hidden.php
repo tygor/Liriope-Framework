3285b21d76d1a9b95c8a97ae5f1ac4fa87c72e5a
Tyler Gordon <tygor81@gmail.com>
2011-12-24 21:53:26 -0500

    renamed README to markdown

---
+++ application/views/default/hidden.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Hidden.php
+ * a hidden page that has no controller
+ * but does have this view file
+ */
+?>
+<?= $header; ?>
+
+<h1>Hidden Page</h1>
+<p>A deceiving name since this page is liked in the navigation. Really, it's a dummy page. A page without brains. I wanted
+to be able to calla a page without a controller, so if a page needs no brain, then here we go!</p>
+<p>1 + 1 = I have no idea!</p>
+
+<?= $footer; ?>
+

51d20dff49b70555290effc0751a67ff958a179a
Tyler Gordon <media@northrockhill.com>
2012-02-09 16:49:10 -0500

    Error page tweaks

---
+++ web/content/error/404.php
@@ -0,0 +1,15 @@
+<?php
+
+// TODO: create a .css version of the LESSCSS error styles
+page::addStylesheet( 'css/error.less', 'stylesheet/less' );
+
+?>
+
+<div id="error404">
+  <section id="message">
+    <h1><span>The Grass is in fact</span> Greener!</h1>
+    <p>But that's not why you're here. Honestly, we can't find the page you want.</p>
+    <a class="home" href="/">Home</a>
+    <a class="contact" href="mailto:webmaster@liriope.local?Subject=Reporting a 404 error">Report</a>
+  </section>
+</div>

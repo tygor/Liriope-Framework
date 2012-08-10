af81d3d9a33b6284a6f87b878dc6432d7fc9d3d0
Tyler Gordon <tygor81@gmail.com>
2012-08-10 10:55:30 -0400

    Custom Error Handler needs to return FALSE
    If the error isn't handled with your custom function, then it needs to return false to allow the default
    handler to take over

--- liriope/library/error.class.php
+++ liriope/library/error.class.php
@@ -39,7 +39,7 @@ class error {
   static function handler( $code, $msg, $file, $line ) {
     if( !( error_reporting() & $code )) {
       // Error code not included in error_reporting
-      return;
+      return FALSE;
     }
 
     switch( $code ) {

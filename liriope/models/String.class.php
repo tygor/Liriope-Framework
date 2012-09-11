164e12e1d165aa580a30f8869b255a9ea3f1d120
Tyler Gordon <tygor81@gmail.com>
2012-09-10 21:36:30 -0400

    Completed the new String model to replace the string static functions

--- liriope/models/String.class.php
+++ liriope/models/String.class.php
@@ -72,7 +72,7 @@ class String {
   public function to_html( $preserve=TRUE ) {
     $i = &$this->getInstance();
     if( $preserve ) {
-      $i = stripslashes( implode( '', preg_replace( '/^([^<].+[^>])$/e', "htmlentities('$1', ENT_COMPAT, 'utf-8')", preg_split('/(<.+?>)/', $i, -1, PREG_SPLIT_DELIM_CAPTURE))));
+      $this->stripslashes(implode('', preg_replace('/^([^<].+[^>])$/e', "htmlentities('$1', ENT_COMPAT, 'utf-8')", preg_split('/(<.+?>)/', $i, -1, PREG_SPLIT_DELIM_CAPTURE))));
     } else {
       $i = htmlentities( $i, ENT_COMPAT, 'utf-8' );
     }

cadd8ba60416e4d19774b472cae3b92c76ca9cb2
Tyler Gordon <tygor81@gmail.com>
2012-11-24 21:41:53 -0500

    Roughed in the ability to serve data from public DropBox pages.

---
+++ site/web/content/dropbox/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$page->title='Drop Box';
+$page->data='2012-11-24';
+
+$file = 'https://dl.dropbox.com/u/51310480/DropboxTest.txt';
+$dropbox = new Dropbox($file);
+
+$yml = new Yaml($dropbox);
+$data = $yml->parse();
+
+?>
+
+<h1><?= $page->title ?></h1>
+
+<pre>
+<?= print_r($data) ?>
+</pre>

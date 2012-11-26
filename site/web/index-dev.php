50ddc842419db919bff8f1ce996bcb0953f8144c
Tyler Gordon <tygor81@gmail.com>
2012-11-25 20:25:29 -0500

    added index-dev.php though it doesn't work

---
+++ site/web/index-dev.php
@@ -0,0 +1,45 @@
+<?php
+
+// --------------------------------------------------
+// Web root of your site
+// --------------------------------------------------
+// this should be the same as the directory where
+// index.php is located.
+//
+$rootWeb = dirname(__FILE__);
+
+// --------------------------------------------------
+// Server root of your site
+// --------------------------------------------------
+// This is where the the meat of the application
+// code lives.
+//
+$root = realpath( $rootWeb . '/../..' );
+
+// --------------------------------------------------
+// Liriope framework folder
+// --------------------------------------------------
+// this is the path to where the liriope framework
+// files are kept. By default it is ../liriope but you
+// can move the folder and change the reference here
+// so that multiple sites can be run from one installation.
+//
+$rootLiriope = $root . '/liriope';
+
+// --------------------------------------------------
+// Site application folder
+// --------------------------------------------------
+// this is the path to where the logic of the site is stored
+// usually /application under which is the /controllers, /models
+// and /views folders.
+//
+$rootApplication = $root . '/site';
+
+// Load Liriope: Monkey Grass
+if( !file_exists( $rootLiriope . '/start.php' )) {
+  die( 'The Liriope framework could not be found.' );
+}
+
+require_once( $rootLiriope . '/start.php' );
+
+?>

6fba88e2b781bb9330441ca57076358b2af4f18c
Tyler Gordon <tygor81@gmail.com>
2013-07-08 17:11:21 -0400

    working on the JS to make autocomplete work

--- site/web/js/script.js
+++ site/web/js/script.js
@@ -52,91 +52,47 @@ function str_rot13( a, map ) {
   return s;
 }
 
-/* --------------------------------------------------
-   Twitter feed widget
-   ----------------------------------------------- */
-function getTweets(user,limit) {
-  // Create a Script Tag
-  var script=document.createElement('script');
-  script.type='text/javascript';
-  script.src= "https://api.twitter.com/1/statuses/user_timeline.json?screen_name="+user+
-    "&count="+limit+
-    "&callback=processTweets&_="+ new Date().getTime();
-  // Add the Script to the Body element, which will in turn load the script and run it.
-  $("body").append(script);
-}
-
-function wrapHash(s) {
-  var hashRegex = /#.+?(\s|$)/gi;
-  var matches = s.match(hashRegex);
-  var tag = '<a href="http://twitter.com/search/?q={0}&src=hash" target="_blank">{1}</a>';
-  if( matches==null ) {
-    return s;
-  }
-  for( var i=0, len = matches.length; i<len; i++) {
-    matches[i] = matches[i].replace(/^\s+|\s+$/g,'');
-    var cleaned = matches[i].replace('#', '%23' );
-    var to = tag.format(cleaned,matches[i]);
-    s = s.replace(matches[i], to);
-  }
-  return s;
-}
-
-function wrapURL(s) {
-  var urlRegex = /(f|ht)tps?:\/\/.+?(\s|$)/gi;
-  var matches = s.match(urlRegex);
-  if( matches==null ) {
-    return s;
-  }
-  for( var i=0, len = matches.length; i<len; i++) {
-    matches[i] = matches[i].replace(/^\s+|\s+$/g,'');
-    var to = '<a href="' + matches[i] + '" target="_blank">' + matches[i]+ "</a>";
-    s = s.replace(matches[i], to);
-  }
-  return s;
-}
-
-function processTweets(jsonData){
-  var shtml = '';
-  if(jsonData){
-    // if there are results (it should be an array), loop through it with a jQuery function
-    var wrapper = "<p class='tweet'><span class='author'><a href='http://twitter.com/{0}' target='_blank'>@{0}</a></span>: {1}</p>";
-    $.each(jsonData, function(index,value){
-      shtml += wrapper.format( value.user.screen_name, wrapHash(wrapURL(value.text)));
-    });
-
-    // Load the HTML in the #tweet_stream div
-    $("#tweets").html( shtml ).addClass('loaded');
-  } else {
-    $("#tweets").html( "Sorry. No tweets were available." );
-  }
-}
-
-String.prototype.format = function() {
-  var args = arguments;
-  return this.replace(/{(\d+)}/g, function(match, number) { 
-    return typeof args[number] != 'undefined'
-      ? args[number]
-      : match
-    ;
-  });
-};
-
+// 
+// SearchBox object
+// used for autocomplete functionality in the page search input boxes
+// 
 var SearchBox = {
-  resultsBox: $('<div class="search-results" style="position: absolute; width: 600px; border: 1px solid #000; background: #eee; z-index: 10;"></div>'),
   url: 'search/autocomplete',
 
   init: function(id) {
+    // get the jQuery version of the passed search input box
     this.inputBox = $(id);
+    // create the div for the results to be dumped into and give them a class
+    resultsBox = document.createElement('div');
+    resultsBox.className = 'search-suggestions';
+    // get the jQuery version of the resultsBox
+    this.resultsBox = $(resultsBox);
+    // get the jQuery version of the parent form element
     this.form = this.inputBox.closest('form');
+    // drop in the resultsBox after the search input and hide it.
     this.inputBox.after(this.resultsBox.hide());
+    // position the resultsBox
+    inputOffset = this.inputBox.position();
+    inputHeight = this.inputBox.css("height").replace(/[^-\d\.]/g, '') * 1;
+console.log(inputOffset.top);
+console.log(inputHeight);
+console.log(inputOffset.top + inputHeight + 'px');
+    this.resultsBox.css('top', inputOffset.top + inputHeight + 'px');
+    this.resultsBox.css('left', inputOffset.left);
+    this.resultsBox.css('width', this.inputBox.css('width'));
+    // when the inputbox is clicked, types into, or changed, get suggestions
     this.inputBox.on('keyup mouseup change', null, this, function(event) {
       event.data.suggest()
     });
+    // when focus is removed from the input box, close the autocomplete box
+    this.inputBox.on('blur', this, function(event) {
+      event.data.closeSuggestions();
+    });
+    // when the results are clicked on, fill the input box with the value clicked
+    // and fire the submit action.
     this.resultsBox.mouseup(this, function(event) {
       event.data.guessSelect(event.target.innerHTML);
     });
-    return this;
   },
   hasQuery: function() {
     if(this.inputBox.val().length===0) {
@@ -151,16 +107,20 @@ var SearchBox = {
         this.resultsBox.slideDown();
       }
     } else {
-      this.resultsBox.slideUp();
+      this.closeSuggestions();
     }
   },
   guessSelect: function(item) {
     this.inputBox.val(item);
     this.form.submit();
+  },
+  closeSuggestions: function () {
+    this.resultsBox.slideUp();
   }
 };
 
 // create a prototypal constructor for our SearchBox object
+// this is the IE8 compatiblie version of the new Object.create() method.
 function searchBox(id) {
   function F() {};
   F.prototype = SearchBox;
@@ -170,12 +130,6 @@ function searchBox(id) {
 }
 
 $(function() {
-  // SearchBox.init('#results-search-input');
-  // SearchBox.init('#search-input');
-  // var search1 = SearchBox.initialize('#search-input');
-  // var search2 = SearchBox.initialize('#results-search-input');
-  var search1 = searchBox('#search-input');
-  search1.init('#search-input');
-  var search2 = searchBox('#results-search-input');
-  search2.init('#results-search-input');
+  var search1 = new searchBox('#search-input');
+  var search2 = new searchBox('#results-search-input');
 });
